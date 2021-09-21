055bcbf34eaa8e7abeceefa73608edc65fb5009f
Bruno Cesar de Oliveira <brunocesardeoliveira2018@gmail.com>
2021-09-21 11:22:26 -0300

    Atualizando a listagem de categorias

--- app.js
+++ app.js
@@ -2,12 +2,29 @@
 const express = require("express");
 const handlebars = require('express-handlebars');
 const admin = require('./routers/admin');
-const mongose = require('mongoose');
 const path = require('path');
+const mongose = require('mongoose');
+const session = require('express-session')
+const flash = require('connect-flash')
+
 const app = express();
 
-
 //Configurações
+//session 
+app.use(session({
+    secret: 'blogapp',
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(flash())
+
+//middleware
+app.use((req,res,next)=>{
+    res.locals.success_msg =req.flash('success_mg')
+    res.locals.error_msg =req.flash('error_mg')
+    next()
+})
+
 //express Parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,11 +36,11 @@ app.set('view engine', 'handlebars');
 //mongose
 mongose.Promise = global.Promise
 mongose.connect('mongodb://localhost/blogapp')
-.then(()=>{
-    console.log('Conectado ao mongo')
-}).catch((err)=>{
-    console.log("Erro ao se conectar:"+err)
-})
+    .then(() => {
+        console.log('Conectado ao mongo')
+    }).catch((err) => {
+        console.log("Erro ao se conectar:" + err)
+    })
 
 //Public 
 app.use(express.static(path.join(__dirname, "public")));
--- routers/admin.js
+++ routers/admin.js
@@ -1,5 +1,9 @@
-const express = require("express");
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
+const mongoose = require('mongoose');
+require('../models/Categoria')
+const Categoria = mongoose.model("categorias")
+
 
 router.get('/', (req, res) => {
     res.render("admin/index")
@@ -10,9 +14,46 @@ router.get('/posts', (req, res) => {
 })
 
 router.get('/categorias', (req, res) => {
-    res.send('Dentro da categoria')
+    Categoria.find().sort({ date: 'desc' })
+        .then((categorias) => {
+            res.render('admin/categorias', { categorias: categorias.map(categorias => categorias.toJSON()) })
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao lista categoria")
+            res.redirect("/admin")
+        })
 })
+
 router.get("/categorias/add", (req, res) => {
     res.render("admin/addcategorias")
+})
+
+router.post("/categorias/nova", (req, res) => {
+
+    var erros = []
+    if (!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null) {
+        erros.push({ texto: "Nome inválido" })
+    }
+    if (!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
+        erros.push({ texto: "Slug inválido" })
+    }
+    if (req.body.nome.length < 2) {
+        erros.push({ texto: "Nome muito pequeno para categoria" })
+    }
+    if (erros.length > 0) {
+        res.render("admin/addcategorias", { erros: erros })
+    } else {
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+        new Categoria(novaCategoria).save()
+            .then(() => {
+                req.flash("success_msg", "Categoria salva com sucesso")
+                res.redirect("/admin/categorias")
+            }).catch((err) => {
+                req.flash("error_msg", "Erro ao salvar categoria" + err)
+                res.redirect("/admin")
+            })
+    }
 })
 module.exports = router
