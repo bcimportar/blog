26da160415c0a2cff9358d5be601236eb3534652
Bruno Cesar de Oliveira <brunocesardeoliveira2018@gmail.com>
2021-09-24 14:07:15 -0300

    Finalizando as categorias e postagem

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const mongoose = require('mongoose');
 const session = require('express-session')
 const flash = require('connect-flash')
 require("./models/Postagem")
+require("./models/Categoria")
 const Postagem = mongoose.model("postagens")
+const Categoria = mongoose.model("categorias")
 
 const app = express();
 
@@ -22,8 +24,8 @@ app.use(flash())
 
 //middleware
 app.use((req, res, next) => {
-    res.locals.success_msg = req.flash('success_mg')
-    res.locals.error_msg = req.flash('error_mg')
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.error_msg = req.flash('error_msg')
     next()
 })
 
@@ -56,6 +58,54 @@ app.get("/", (req, res) => {
         }).catch((err) => {
             req.flash("error_msg", "Houve um ero interno")
             // res.redirect("/404")
+        })
+})
+
+app.get("/postagem/:slug", (req, res) => {
+    Postagem.findOne({ slug: req.params.slug })
+        .then((postagem) => {
+            if (postagem) {
+                res.render("postagem/index", { postagem: postagem.toJSON() })
+            } else {
+                req.flash("error_msg", "Está postagem não existe")
+                res.redirect("/")
+            }
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/")
+        })
+})
+
+app.get("/categorias", (req, res) => {
+    Categoria.find()
+        .then((categorias) => {
+            res.render("categorias/index", { categorias: categorias.map(categorias => categorias.toJSON()) })
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno ao listar categorias")
+            res.redirect("/")
+        })
+})
+
+app.get("/categorias/:slug", (req, res) => {
+
+    Categoria.findOne({ slug: req.params.slug })
+        .then((categoria) => {
+            if (categoria) {
+                Postagem.find({ categoria: categoria._id })
+                    .then((postagens) => {
+                        res.render("categorias/postagem", { postagens: postagens.map(postagens => postagens.toJSON()), categorias: categoria.toJSON() })
+                    })
+                    .catch((err) => {
+                        req.flash("error_msg", "Houve um erro ao listar os posts")
+                        res.redirect("/")
+                    })
+            } else {
+                req.flash("error_msg", "Está categoria não existe")
+                res.redirect("/")
+            }
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno ao carregar a página desta  categoria")
+            res.redirect("/")
         })
 })
 
